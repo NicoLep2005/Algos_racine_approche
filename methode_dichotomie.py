1a6187c890b0202f19fdf7e6e63abf98680a054c
NicoLep2005 <66734165+NicoLep2005@users.noreply.github.com>
2021-02-17 21:39:18 +0100

    Update methode_dichotomie.py

--- methode_dichotomie.py
+++ methode_dichotomie.py
@@ -12,12 +12,12 @@ from math import *
 def g(x):
     return log(x) + x**2
 
-#algo dichotomique pour fonction continue, basée sur le corollaire du TVI
+#algo dichotomique pour fonction continue et monotone, basée sur le corollaire du TVI
 
 def methode_dichotomie(a,b):
     half = (a+b)/2
-    if (b-a) <= 10**-2:
-        return round(half, 2)
+    if (b-a) <= 10**-2:             #on indique ici la précision voulue
+        return round(half, 2)       #on arrondit le résultat à x décimales près (on peut considérer ne pas être plus précis que demandé au départ)
     else:
         if g(half)*g(b)>0:
             return methode_dichotomie(a, half)
